d9a5644b0b20bf60b23be72c0773a5e6e0fddb67
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-16 08:19:20 +0330

    commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,3 +1,5 @@
+const Task = require('../models/task')
+
 const getAllTasks = (req , res) =>{
     res.send('get all tasks')
 }
@@ -5,8 +7,11 @@ const getAllTasks = (req , res) =>{
 const getTask = (req , res ) =>{
     res.json({ id : req.params.id })
 }
-const createTask = (req , res ) =>{
-    res.json(req.body)
+const createTask = async (req , res ) =>{
+
+    const task = await Task.create(req.body)
+
+    res.status(201).json({task})
 }
 
 const updateTask  = (req , res) =>{
