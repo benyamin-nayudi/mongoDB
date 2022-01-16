a6028dc2427d6eb8eb5a480d053bf8075d6d702a
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-16 18:10:56 +0330

    error handler

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const tasks = require('./routes/tasks')
 const connectDB =  require('./db/connect')
+const notFound = require('./middleware/notFound')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
 require('dotenv').config()
+
 
 const app = express();
 
 app.use(express.static('./public'))
 app.use(express.json())
 
-// app.get('/hello' , (req , res) =>{
-//     res.send('task manager app')
-// })
-
 app.use('/api/v1/tasks' , tasks)
 
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,64 +1,52 @@
 const Task = require('../models/task')
+const asyncWrapper = require('../middleware/async')
+
+
+
 
 
 // get all tasks
-const getAllTasks = async (req , res) =>{
-    try{
+const getAllTasks = asyncWrapper( async (req , res) =>{
         const tasks = await Task.find({}) //get all the document
         res.status(200).json({tasks})
-    }catch(err){
-        res.status(500).json({msg: error}) //general server error 
-    }
-}
+})
+
 
 
 // get a single task
-const getTask = async (req , res ) =>{
-    try {
-        const {id : taskID } = req.params
-        const task = await Task.findOne({_id:taskID})
-        if(!task)  return res.status(404).json({ msg: `No task with id ${taskID}`   })
-        res.status(200).json({task })
-    } catch (error) {
-        res.status(500).json({massege: error}) //general server error 
+const getTask = asyncWrapper( async (req , res ) =>{
+            const {id : taskID } = req.params
+            const task = await Task.findOne({_id:taskID})
+            if(!task)  return res.status(404).json({ msg: `No task with id ${taskID}`   })
+            res.status(200).json({task })
     }
-}
+)
 
 
 // post
-const createTask = async (req , res ) =>{
-    try{
+const createTask = asyncWrapper(
+    async (req , res ) =>{
         const task = await Task.create(req.body)   
         res.status(201).json({task})
-    }catch(error){
-        res.status(500).json({msg: error}) //general server error 
-    }
-}
+})
 
 
 
 // delete
-const deleteTask =async (req , res) =>{
-    // res.send('delete Task')
-
-    const {id : taskID } = req.params
-
-    try{
+const deleteTask =asyncWrapper(
+    async (req , res) =>{
+        const {id : taskID } = req.params
         const task = await Task.findOneAndDelete({_id : taskID})
-
         if(!task) return res.status(404).json({msg: 'there is no such id'})
-
         res.status(200).json({task})
-    }catch(error){
-        res.status(500).json({msg: error})
     }
-}
+)
 
 
 // put
-const updateTask  =async (req , res) =>{
+const updateTask  =asyncWrapper(
+    async (req , res) =>{
     
-    try{
         const {id : taskID} = req.params
         const task = await Task.findByIdAndUpdate({_id: taskID } , req.body , {
             new: true,
@@ -68,11 +56,9 @@ const updateTask  =async (req , res) =>{
         if(!task) return res.status(404).json({msg: 'there is no task with this id'}) 
         
         res.status(200).json({task})
-    }catch(error){
-        res.status(500).json({msg: error}) 
     }
-}
-
+    
+)
 
 
 
